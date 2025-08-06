bbdceb1ec8246fa285c8a2bd03ed12e0f8ea398d
AngelSalazarV <angel152265@gmail.com>
2025-08-06 16:53:29 -0500

    add routing and authentication pages: implement login and register components; update App component; add react-router-dom dependency

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,25 +1,28 @@
-
-export function LoginPage() {
+export function RegisterPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="flex flex-col bg-white p-6 rounded-2xl text-black">
         <div className="flex items-center justify-center ">
-          <h2>Login</h2>
+          <h2>Register</h2>
         </div>
         <form className="flex flex-col gap-4 mt-4">
           <label htmlFor="username">Username:</label>
           <input className="bg-gray-300 rounded-2xl text-black p-1" type="text" name="username" required />
-          
+
+          <label htmlFor="email">Email:</label>
+          <input className="bg-gray-300 rounded-2xl text-black p-1" type="email" name="email" required />
+
           <label htmlFor="password">Password:</label>
           <input className="bg-gray-300 rounded-2xl text-black p-1" type="password" name="password" required />
 
-          <button className="bg-blue-500 text-white rounded-2xl p-1 cursor-pointer hover:bg-blue-600" type="submit">Login</button>
+          <button className="bg-blue-500 text-white rounded-2xl p-1 cursor-pointer hover:bg-blue-600" type="submit">Register</button>
         </form>
         <div className="flex flex-col items-center mt-4">
-          <p>Don't have an account?</p>
-          <a href="/register" className="text-blue-500 hover:underline">Register</a>
+          <p>Already have an account?</p>
+          <a href="/login" className="text-blue-500 hover:underline">Login</a>
         </div>
       </div>
     </div>
   );
 }
+ 
---
+++ frontend/src/services/AuthServices.js
@@ -0,0 +1,39 @@
+export const loginService = async (email, password) => {
+  try {
+    const response = await fetch('http://127.0.0.1:5000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    })
+    if(!response.ok) {
+      throw new Error('Login failed')
+    }
+    const data = await response.json()
+    return data
+  }catch (error) {
+    console.error('Error during login:', error)
+    throw error
+  }
+}
+
+export const registerService = async (email, password) => {
+  try {
+    const response = await fetch('http://127.0.0.1:5000/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    })
+    if(!response.ok) {
+      throw new Error('Registration failed')
+    }
+    const data = await response.json()
+    return data
+  }catch (error) {
+    console.error('Error during registration:', error)
+    throw error
+  }
+}
